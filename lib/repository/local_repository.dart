e2ff7498aca3da3372a42ab872473a5e3e396ea0
toankx <toankxt@gmail.com>
2020-12-02 20:07:12 +0700

    update by toankx

--- lib/repository/local_repository.dart
+++ lib/repository/local_repository.dart
@@ -1,3 +1,7 @@
+import 'package:mynote/ui/views/note/note_model.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 class LocalRepository {
   /// Xây dựng một hàm tạo private
   LocalRepository._internal();
@@ -6,5 +10,25 @@ class LocalRepository {
   static final _cache = <String, LocalRepository>{};
 
   static LocalRepository get instance =>
-      _cache.putIfAbsent('LocalPersi', () => LocalRepository._internal());
+      _cache.putIfAbsent('LocalPersistence', () => LocalRepository._internal());
+
+  bool isInitialized = false;
+
+  Database _db;
+
+  Future<Database> db() async {
+    if (!isInitialized) await _init();
+    return _db;
+  }
+
+  Future _init() async {
+    var databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'db_note_app.db');
+
+    _db = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute(Note.createTable);
+    });
+    isInitialized = true;
+  }
 }
